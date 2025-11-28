fb9b63fc22d7dfaafdf6702473576c54e0709341
Kenza Houben <kenza.tabti@student.ehb.be>
2025-11-28 09:47:19 +0100

    Postman tested

--- backend/backend.js
+++ backend/backend.js
@@ -1,11 +1,10 @@
- //const express = require("express"); can use only if you don't add "type" = "module"
 import express from "express";
 import { readFile, writeFile } from "node:fs/promises";
 const app = express();
 const port = 3000;
  
 app.use(express.static("public"));
-app.use(express.json()); //alle data van en naar de api is uniek
+app.use(express.json());
  
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
@@ -13,37 +12,18 @@ app.listen(port, () => {
  
 app.use(express.static("public"));
  
-app.get("/api/films", async (req, res) => {
+app.get("/data/films", async (req, res) => {
     const contents = await readFile("data/films.json", { encoding: "utf8" });
     const data = JSON.parse(contents);
     res.json(data);
 });
  
-//app.get("/api/boardgame", (req,res) => {
-//    res.send("test");
-//})
-
-// duplicated the same one on accident
-
-// app.get("/api/films", async (req, res) => {
-//     // To get individually each id of the films
-//     let id = req.query.id;
-//     // Reads the file
-//     const contents = await readFile("data/films.json", { encoding: "utf8" });
-//     const data = JSON.parse(contents);
-//     //get one film
-//     let films = data[id]; 
-//     res.json(films);
-// });
- 
-app.post("/api/films", async (req, res) => {
+app.post("/data/films", async (req, res) => {
     
     let data = req.body;
 
     console.log(data)
  
-    // const fileName = data.name.replaceAll(" ", "");
-    // await writeFile(`data/${fileName}.json`, JSON.stringify(data));
     res.send("succces");
 });
  
