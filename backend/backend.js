f8389a2b907266d93fea640a687edd23ff37e2ad
Kenza Houben <kenza.tabti@student.ehb.be>
2025-11-28 10:48:10 +0100

    comments added

--- backend/backend.js
+++ backend/backend.js
@@ -6,6 +6,12 @@ const port = 3000;
 // const secret_password = process.env.SECRET_PASSWORD // 'test;
 
 //21:22 -> gitignore
+
+// TODO: backend folder wegdoen
+
+// https://www.youtube.com/live/_OFH6RX9G4s -> timestamp = 23:09
+
+// render: https://dashboard.render.com/web/srv-d4kmabmuk2gs73fucsc0/events
  
 app.use(express.static("public"));
 app.use(express.json());
